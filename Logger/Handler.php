ff343ca0b19b556a217fbb095840acdda169a6e1
Randy Esplin <randy.esplin@gmail.com>
2026-02-06 11:32:03 -0800

    Update Logger/Handler

--- Logger/Handler.php
+++ Logger/Handler.php
@@ -9,14 +9,14 @@ namespace ECInternet\Base\Logger;
 
 use Magento\Framework\Filesystem\Driver\File as FileDriver;
 use Magento\Framework\Logger\Handler\Base as LoggerHandlerBase;
-use Monolog\Level;
+use Monolog\Logger;
 
 /**
  * Logger handler
  */
 class Handler extends LoggerHandlerBase
 {
-    protected $loggerType = Level::Info;
+    protected $loggerType = Logger::INFO;
 
     protected $fileName = '/var/log/ECInternet_Base.log';
 
